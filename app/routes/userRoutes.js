3c6dd797ad9728f441393b32c57f89cef4ee03b0
rajat65018 <rb465629@gmail.com>
2024-03-03 15:02:10 +0530

    fea:getting the buffer data using the multer

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,13 +1,15 @@
 const multer = require('multer');
 const express = require('express');
 
-const upload = multer({ dest: "./uploads" });
+// const upload = multer({ dest: "./uploads" });
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const userRoutes = express.Router();
 
 userRoutes.post('/signup', upload.single('avatar'), (req, res) => {
-    console.log(req.files,'file file');
-    res.status(200).json({ message: 'Data submitted successfully' });
+    console.log(req.file,'file file');
+    res.status(200).json({ message: req.file });
 })
 
 module.exports = userRoutes;
